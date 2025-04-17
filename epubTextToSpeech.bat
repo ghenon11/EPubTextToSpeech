54123e1b074c38719c0fe4b26f13a9fd5b43051e
Guillaume <127008717+ghenon11@users.noreply.github.com>
2025-04-17 11:46:02 +0200

    initial version

--- epubTextToSpeech.bat
+++ epubTextToSpeech.bat
@@ -2,4 +2,4 @@ echo off
 set TF_ENABLE_ONEDNN_OPTS=0
 rem python "epubTextToSpeech v3.py" > logs\epubTextToSpeech_Launcher.log 2>&1
 echo on
-python "epubTextToSpeech v5.py"
+python "epubTextToSpeech.py"
