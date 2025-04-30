8f04843e5169e6511d5903b204b260617cefc7c1
Guillaume <127008717+ghenon11@users.noreply.github.com>
2025-04-30 05:45:54 +0200

    small cleanup

--- freeze.py
+++ freeze.py
@@ -1,22 +1,21 @@
+import sklearn
+import torch
+import nltk
+from cx_Freeze import setup, Executable
+import os
 import sys
 sys.setrecursionlimit(10000)
-import os
-from cx_Freeze import setup, Executable
-import nltk
-import torch
-import sklearn
-import shutil
 
 nltk.download('punkt')
 try:
     punkt_path = str(nltk.data.find('tokenizers/punkt'))
 #    include_files.append((punkt_path, 'nltk_data/tokenizers/punkt'))
-except LookupError:   
+except LookupError:
     print("Error: punkt tokenizer not found. Run nltk.download('punkt') before building.")
 
 torch_lib_dir = os.path.join(os.path.dirname(torch.__file__), 'lib')
 sklearn_lib_dir = os.path.join(os.path.dirname(sklearn.__file__), ".libs")
-    
+
 # Define the directories you want to include
 include_directories = [
     ('eSpeak NG', 'eSpeak NG'),  # (source_dir, target_dir)
@@ -24,13 +23,13 @@ include_directories = [
     ('Epub', 'Epub'),
     ('Media', 'Media'),
     ('Models', 'Models'),
-    ('StyleTTS2', 'StyleTTS2'),
-    ('StyleTTS2', os.path.join("lib","StyleTTS2")),
+    (os.path.join("StyleTTS2", "Utils"), os.path.join("StyleTTS2", "Utils")),
+    ('StyleTTS2', os.path.join("lib", "StyleTTS2")),
     ('Imgs', 'Imgs'),
-    (punkt_path, os.path.join("nltk_data","tokenizers","punkt","PY3")),
-    (punkt_path, os.path.join("nltk_data","tokenizers","punkt")),
-    (torch_lib_dir, os.path.join("lib","torch","lib")),
-    (sklearn_lib_dir, os.path.join("lib","sklearn",".libs"))
+    (punkt_path, os.path.join("nltk_data", "tokenizers", "punkt", "PY3")),
+    (punkt_path, os.path.join("nltk_data", "tokenizers", "punkt")),
+    (torch_lib_dir, os.path.join("lib", "torch", "lib")),
+    (sklearn_lib_dir, os.path.join("lib", "sklearn", ".libs"))
 ]
 
 # Create a list of files to include
@@ -38,32 +37,35 @@ include_files = []
 for source_dir, target_dir in include_directories:
     for root, dirs, files in os.walk(source_dir):
         for file in files:
-            include_files.append((os.path.join(root, file), os.path.join(target_dir, os.path.relpath(root, source_dir), file)))
-            
+            include_files.append((os.path.join(root, file), os.path.join(
+                target_dir, os.path.relpath(root, source_dir), file)))
+
 include_files.append("config.ini")
 include_files.append("epubTextToSpeech.bat")
 
-include_files.append((torch_lib_dir, os.path.join("lib","torch","lib")))
-include_files.append((sklearn_lib_dir, os.path.join("lib","sklearn",".libs")))
+# include_files.append((torch_lib_dir, os.path.join("lib","torch","lib")))
+# include_files.append((sklearn_lib_dir, os.path.join("lib","sklearn",".libs")))
 
-#executables = [Executable("epubTextToSpeech.py", base = 'Win32GUI', icon = 'D:\Python\Epubtextspeech\Imgs\voice_presentation.ico')]
-executables = [Executable("epubTextToSpeech.py", icon = 'D:\Python\Epubtextspeech\Imgs\voice_presentation.ico'),"setup.py"]
+# executables = [Executable("epubTextToSpeech.py", base = 'Win32GUI', icon = 'D:\Python\Epubtextspeech\Imgs\voice_presentation.ico')]
+executables = [Executable(
+    "epubTextToSpeech.py", icon='D:\Python\Epubtextspeech\Imgs\voice_presentation.ico'), "setup.py"]
 
 
 build_exe_options = {
-    "packages": ["transformers","torch","nltk","numpy","tokenizers","phonemizer","scipy","customtkinter","tkinter","numba","sklearn"],
+    "packages": ["transformers", "torch", "nltk", "numpy", "tokenizers", "phonemizer", "scipy", "customtkinter", "tkinter", "numba", "sklearn"],
     "excludes": [],
     "include_msvcr": True,
     "include_files": include_files
 }
 
-print(f"Running cx_freeze with build_options [{build_exe_options}] and executables [{executables}]")
+print(
+    f"Running cx_freeze with build_options [{build_exe_options}] and executables [{executables}]")
 
 # Calling setup function
 setup(
-    name = "EPub Text To Speech",
-    version = "0.7",
-    description = "Synthetize text from epub or pdf and then play audio with synchronized reading",
+    name="EPub Text To Speech",
+    version="0.7",
+    description="Synthetize text from epub or pdf and then play audio with synchronized reading",
     options={"build_exe": build_exe_options},
-    executables = executables,
+    executables=executables,
 )
--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 import os
 import sys
 
+
 def replace_strings_in_files(directory, replacements):
 
     for root, dirs, files in os.walk(directory):
@@ -21,27 +22,31 @@ def replace_strings_in_files(directory, replacements):
 
                     if search_string in content:
 
-                        content = content.replace(search_string, replace_string)
+                        content = content.replace(
+                            search_string, replace_string)
 
-                        print(f'Replaced "{search_string}" with "{replace_string}" in {file_path}')
+                        print(
+                            f'Replaced "{search_string}" with "{replace_string}" in {file_path}')
 
                 with open(file_path, 'w', encoding='utf-8') as f:
 
                     f.write(content)
-                
+
                 with open(file_path, 'r', encoding='utf-8') as f:
 
                     content = f.read()
 
                 for search_string, replace_string in replacements.items():
-                    search_string=search_string.replace("\\","\\\\")
-                    replace_string=replace_string.replace("\\","\\\\")
+                    search_string = search_string.replace("\\", "\\\\")
+                    replace_string = replace_string.replace("\\", "\\\\")
 
                     if search_string in content:
 
-                        content = content.replace(search_string, replace_string)
+                        content = content.replace(
+                            search_string, replace_string)
 
-                        print(f'Replaced "{search_string}" with "{replace_string}" in {file_path}')
+                        print(
+                            f'Replaced "{search_string}" with "{replace_string}" in {file_path}')
 
                 with open(file_path, 'w', encoding='utf-8') as f:
 
@@ -49,21 +54,21 @@ def replace_strings_in_files(directory, replacements):
 
 # Define the directory to search and the strings to replace
 
+
 directory_to_search = '.'
 INSTALL_DIR = os.path.dirname(sys.executable)
 
 replacements = {
 
-    "D:\\Python\\Epubtextspeech\\Models": os.path.join(INSTALL_DIR,"Models"),
-    "D:\Python\\Epubtextspeech\\Epub": os.path.join(INSTALL_DIR,"Epub"),
-    "D:\\Python\\Epubtextspeech\\StyleTTS2": os.path.join(INSTALL_DIR,"StyleTTS2")
+    "D:\\Python\\Epubtextspeech\\Models": os.path.join(INSTALL_DIR, "Models"),
+    "D:\Python\\Epubtextspeech\\Epub": os.path.join(INSTALL_DIR, "Epub"),
+    "D:\\Python\\Epubtextspeech\\StyleTTS2": os.path.join(INSTALL_DIR, "StyleTTS2")
 
 }
 
 # Call the function
-
+os.makedirs("logs", exist_ok=True)
 replace_strings_in_files(directory_to_search, replacements)
 
 print("\n\nPress any key to end...")
 input()
-
